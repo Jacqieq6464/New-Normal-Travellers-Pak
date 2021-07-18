e0b6b74ef85c515a3c303170bf509f72309266d4
Cgordon-bee <81937022+Cgordon-bee@users.noreply.github.com>
2021-07-18 11:04:32 +0100

    added basic nav bar & js eventlistener

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+var burgerIcon = document.querySelector("#burger");
+var navbarmenu =document.querySelector("#nav-links");
+
+burgerIcon.addEventListener("click", () => {
+
+    navbarmenu .classList.toggle("is-active");
+
+});
+
+
