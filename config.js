bea9d2a712aaa1f5f39fc6c375b5386ce992b7b2
jacqie williamson <jacqieq@me.com>
2021-07-23 00:36:33 +0100

    API KEYS make secret

---
+++ config.js
@@ -0,0 +1,6 @@
+var config = {
+    SECRET_API_KEY:'8105c2770c2e434bb2d16410a874ae21',// Geoapify API
+    SECRET_API_KEY2:'5b3345b556ab34ec58c3645edd09ccb0',//PositionStack API
+}
+
+export default config
