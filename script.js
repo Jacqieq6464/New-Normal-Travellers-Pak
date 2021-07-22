711e1359cdf5efedf19e6a5aa5ebc4754dbe7cfd
Cgordon-bee <81937022+Cgordon-bee@users.noreply.github.com>
2021-07-22 23:00:44 +0100

    Add cards to footer

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
+// 
 
-document.addEventListener('DOMContentLoaded', () => {
 
-    // Get all "navbar-burger" elements
-    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  var signUpButton =document.getElementById("signUp");
+  var searchBar = document.getElementById ("searchBar");
+  var login = document.getElementById ("login");
+
   
-    // Check if there are any navbar burgers
-    if ($navbarBurgers.length > 0) {
   
-      // Add a click event on each of them
-      $navbarBurgers.forEach( el => {
-        el.addEventListener('click', () => {
+  signUp.addEventlistener ("click", e => {
+
+  }, {
+    // I want to use the event capturing element to eliminate any conflict in bubbling
+  capture: true
+  })
+
   
-          // Get the target from the "data-target" attribute
-          const target = el.dataset.target;
-          const $target = document.getElementById(target);
+
+
+
+
+
   
-          // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-          el.classList.toggle('is-active');
-          $target.classList.toggle('is-active');
-  
-        });
-      });
-    }
-  
-  });
