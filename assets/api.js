1d2d4534f9261e5d200bda49de5b6604c03e84fb
jacqie williamson <jacqieq@me.com>
2021-07-23 23:29:16 +0100

    file disabled in html

--- assets/api.js
+++ assets/api.js
@@ -1,5 +1,21 @@
 import config from "../config";//makes config available 
 
 
-var API_KEY1 = config.SECRET_API_KEY;
-var API_KEY2 = config.SECRET_API_KEY2;
+var API_KEY1 = config.SECRET_API_KEY;// Geoapify API KEY
+var API_KEY2 = '5b3345b556ab34ec58c3645edd09ccb0';//PositionStack API
+var city = "london";
+var categories = categories;
+
+
+fetch(`http://api.positionstack.com/v1/forward?${API_KEY2}&query=${city}`)
+.then(response => {
+    
+    var result = response.json();
+    console.log (result);
+})
+
+// .then 
+
+// fetch(`http://api.positionstack.com/v1/forward?${API_KEY1}&query=${city}`)
+// .then(response => {return response.json();
+// })
